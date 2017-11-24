33cb1edf53f55c12ef84e019f0842401497ea717
wenhu <wenhua.yan@gmail.com>
2017-11-23 22:56:39 -0600

    spring web service working with angular 4 client

---
+++ src/main/java/com/example/demo/ProductSvc.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/svc")
+public class ProductSvc {
+	List<Product> result;
+	
+	@RequestMapping(path="/products", produces="application/json")
+	public Product[] getProducts() {
+		if( result == null ) {
+			result = new ArrayList<Product>();
+			
+			result.add( new Product(1, "Egg", "Meat", 2.59) );
+			result.add( new Product(2, "Chips", "Cracker", 3.59) );
+			result.add( new Product(3, "Hat", "Cloth", 12) );
+			result.add( new Product(4, "Camera", "Electronic", 1250) );
+		}
+		
+		return result.toArray(new Product[0]);
+	}
+	
+	
+}
